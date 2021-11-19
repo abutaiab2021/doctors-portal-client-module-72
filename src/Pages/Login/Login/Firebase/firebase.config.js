1252cbfd1010343339bf4c0a6b9cc1ce68811d2a
abutaiab2021 <abutaiabcse95@gmail.com>
2021-11-19 20:15:26 +0600

    doctor portal website push in github

---
+++ src/Pages/Login/Login/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+        apiKey: "AIzaSyDp0jAYvx06kGb7kT5IbW9taqY4o6khqSM",
+        authDomain: "doctors-portal-client-f1fff.firebaseapp.com",
+        projectId: "doctors-portal-client-f1fff",
+        storageBucket: "doctors-portal-client-f1fff.appspot.com",
+        messagingSenderId: "603890142090",
+        appId: "1:603890142090:web:619ac5198d67edbac25673"
+};
+export default firebaseConfig;
