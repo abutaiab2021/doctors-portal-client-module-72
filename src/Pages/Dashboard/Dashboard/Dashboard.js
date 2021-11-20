e575faffaf9b2132aa8afd8fae7b740a1c0f97bb
abutaiab2021 <abutaiabcse95@gmail.com>
2021-11-21 00:07:45 +0600

    add doctors page create

--- src/Pages/Dashboard/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard/Dashboard.js
@@ -29,6 +29,7 @@ import DashboardHome from '../DashboardHome/DashboardHome';
 import AddDoctor from '../AddDoctor/AddDoctor';
 import useAuth from '../../../hooks/useAuth';
 import AdminRoute from '../../Login/AdminRoute/AdminRoute';
+import Payment from '../Payment/Payment';
 
 const drawerWidth = 200;
 
@@ -132,6 +133,9 @@ function Dashboard(props) {
                 <Switch>
                     <Route exact path={path}>
                         <DashboardHome></DashboardHome>
+                    </Route>
+                    <Route  path={`${path}/payment/:appointmentId`}>
+                        <Payment></Payment>
                     </Route>
                     <AdminRoute path={`${path}/makeAdmin`}>
                         <MakeAdmin></MakeAdmin>
---
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { loadStripe } from '@stripe/stripe-js';
+import CheckoutForm from './CheckoutForm';
+import { Elements } from '@stripe/react-stripe-js';
+
+const stripePromise =  loadStripe('pk_test_51JxYkpGG89s6Z8uWYAI67afb07TnYCnG6ymMhwF08pylJLLV34QeKmvwpMaPJjbIfmBrbtEmsb0oLgveq0KQwFmn00lDrJ7Ly8')
+
+const Payment = () => {
+    const { appointmentId } = useParams();
+    const [appointment, setAppointment] = useState({});
+    useEffect(() => {
+        fetch(`http://localhost:5000/appointments/${appointmentId}`)
+            .then(res => res.json())
+            .then(data => setAppointment(data));
+    }, [appointmentId]);
+    return (
+        <div>
+            <h2>Please Pay for: {appointment.patientName} for {appointment.serviceName}</h2>
+            <h4>Pay: ${appointment.price}</h4>
+            {appointment?.price && <Elements stripe={stripePromise}>
+                <CheckoutForm
+                    appointment={appointment}
+                />
+            </Elements>}
+        </div>
+    );
+};
+
+export default Payment;
+
+/*
+1. install stripe and stripe-react
+2. set publishable key
+3. Elements
+4. Checkout Form
+-----
+5. Create payment method
+6. server: create payment Intent api
+7. Load client secret
+8. ConfirmCard payment
+9. handle user error
+*/
